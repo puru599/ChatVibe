2119a8e65c1e11aae3eee64bb75a6df7a850ec21
SHA43F <shareefpmk44@gmail.com>
2023-01-28 21:03:50 +0530

    4.3) Frontend - Backend Connection

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { useEffect, useState } from "react";
 import SignUp from "./pages/SingUp/SignUp";
 
 function App() {
-  const [heading, setHeading] = useState("");
-  const getHeading = async () => {
-    const response = await axios.get("/home");
-    setHeading(response.data.text);
-  };
-  useEffect(() => {
-    getHeading();
-  }, []);
+  // const [heading, setHeading] = useState("");
+  // const getHeading = async () => {
+  //   const response = await axios.get("/home");
+  //   setHeading(response.data.text);
+  // };
+  // useEffect(() => {
+  //   getHeading();
+  // }, []);
   return (
     <div>
       <SignUp />
--- src/pages/SingUp/SignUp.js
+++ src/pages/SingUp/SignUp.js
@@ -1,60 +1,83 @@
-import { useRef, useState } from "react";
+import React, { useRef, useState } from "react";
+import axios from "axios";
 import { useHistory } from "react-router-dom";
 import Form from "../../Layout/UI/Form";
 import Button from "../../Layout/UI/Button";
 
 const SignUp = () => {
+  const history = useHistory("");
   const nameRef = useRef("");
   const emailRef = useRef("");
   const phoneRef = useRef("");
   const pswdRef = useRef("");
+  const [error, setError] = useState(false);
 
   const signUpSubmitHandler = async (event) => {
     event.preventDefault();
-    const nameValue = nameRef.current.value;
-    const emailValue = emailRef.current.value;
-    const phoneValue = phoneRef.current.value;
-    const pswdValue = pswdRef.current.value;
-
-    console.log(nameValue, emailValue, phoneValue, pswdValue);
+    const data = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      phone: phoneRef.current.value,
+      password: pswdRef.current.value
+    };
+    try {
+      const response = await axios.post("http://localhost:5000/signUp", data, {
+        headers: { "content-type": "application/json" }
+      });
+      if (response.status !== 201) {
+        throw new Error(response);
+      } else {
+        history.push("/signIn");
+      }
+    } catch (error) {
+      if (error.response.data.errors[0].value === data.email) {
+        setError("User Email Already Exists, Please Login...");
+      }
+      if (error.response.data.errors[0].value === data.phone) {
+        setError("User Phone Number Already Exists, Please Login...");
+      }
+    }
   };
   return (
-    <Form onSubmit={signUpSubmitHandler}>
-      <div>
-        <h3>Sign Up</h3>
-      </div>
-      <div>
-        <input
-          id="nameId"
-          placeholder="Username"
-          type="text"
-          ref={nameRef}
-          required
-        ></input>
-        <input
-          id="emailId"
-          placeholder="Email"
-          type="text"
-          ref={emailRef}
-          required
-        ></input>
-        <input
-          id="phnId"
-          placeholder="Phone Number"
-          type="tel"
-          ref={phoneRef}
-          required
-        ></input>
-        <input
-          id="passwordId"
-          placeholder="Password"
-          type="password"
-          ref={pswdRef}
-          required
-        />
-      </div>
-      <Button>Sign Up</Button>
-    </Form>
+    <React.Fragment>
+      <Form onSubmit={signUpSubmitHandler}>
+        <div>
+          <h3>Sign Up</h3>
+        </div>
+        <div>
+          <input
+            id="nameId"
+            placeholder="Username"
+            type="text"
+            ref={nameRef}
+            required
+          ></input>
+          <input
+            id="emailId"
+            placeholder="Email"
+            type="text"
+            ref={emailRef}
+            required
+          ></input>
+          <input
+            id="phnId"
+            placeholder="Phone Number"
+            type="tel"
+            ref={phoneRef}
+            required
+          ></input>
+          <input
+            id="passwordId"
+            placeholder="Password"
+            type="password"
+            ref={pswdRef}
+            required
+          />
+        </div>
+        <Button>Sign Up</Button>
+      </Form>
+      {error ? <h4>{error}</h4> : null}
+    </React.Fragment>
   );
 };
 
